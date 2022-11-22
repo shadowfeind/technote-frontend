922f88b22677a4ce3fb4a25f5526eba61138e9b0
Kiran Rai <travestroy007@gmail.com>
2022-11-22 20:52:01 +0545

    making layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import { Route, Routes } from "react-router-dom";
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
-import Login from "./components/Login";
 import Public from "./components/Public";
+import Login from "./features/auth/Login";
+import DashLayout from "./components/DashLayout";
+import Welcome from "./features/auth/Welcome";
+import NotesList from "./features/notes/NotesList";
+import UsersList from "./features/users/UsersList";
 
 function App() {
   return (
@@ -10,6 +13,19 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Public />} />
         <Route path="login" element={<Login />} />
+
+        <Route path="dash" element={<DashLayout />}>
+          <Route index element={<Welcome />} />
+
+          <Route path="notes">
+            <Route index element={<NotesList />} />
+          </Route>
+
+          <Route path="users">
+            <Route index element={<UsersList />} />
+          </Route>
+        </Route>
+        {/* End Dash */}
       </Route>
     </Routes>
   );
